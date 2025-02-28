3e7e203f5701cd37868975e23b217c2838861ee6
haiyl <haiyl@scudpower.com>
2025-02-28 22:43:51 +0800

    fix: 修改mybatis-puls 和 springboot版本的兼容

--- smart-mirror-api/src/main/java/cn/haiyinlong/smart/mirror/infrastructure/config/MybatisPlusConfig.java
+++ smart-mirror-api/src/main/java/cn/haiyinlong/smart/mirror/infrastructure/config/MybatisPlusConfig.java
@@ -1,12 +1,12 @@
 package cn.haiyinlong.smart.mirror.infrastructure.config;
 
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
 /**
  * MybatisPlusConfig
@@ -20,7 +20,7 @@ public class MybatisPlusConfig {
     @ConditionalOnMissingBean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         final MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
         return interceptor;
     }
 }
